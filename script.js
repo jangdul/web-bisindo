12e610d8f2ea37ab1ad668e2f16c8b7487cbadce
jangdul <dajaljalal@gmail.com>
2026-02-19 17:45:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
-function daftar() {
-    let nama = document.getElementById("nama").value;
-    let email = document.getElementById("email").value;
-
-    if(nama === "" || email === "") {
-        alert("Isi semua data!");
-        return;
-    }
-
-    let peserta = JSON.parse(localStorage.getItem("peserta")) || [];
-    peserta.push({nama: nama, email: email});
-    localStorage.setItem("peserta", JSON.stringify(peserta));
-
-    alert("Pendaftaran berhasil!");
-}
-
-function login() {
-    let email = document.getElementById("loginEmail").value;
-    let peserta = JSON.parse(localStorage.getItem("peserta")) || [];
-
-    let ditemukan = peserta.find(p => p.email === email);
-
-    if(ditemukan) {
-        window.location.href = "menu.html";
-    } else {
-        alert("Email tidak ditemukan!");
-    }
-}
-
 function bukaVideo(jenis) {
     let container = document.getElementById("videoContainer");
 
-    if(jenis === "video1") {
-        container.innerHTML = `<iframe width="300" src="https://www.youtube.com/embed/VIDEO_ID_1"></iframe>`;
-    } else if(jenis === "video2") {
-        container.innerHTML = `<iframe width="300" src="https://www.youtube.com/embed/VIDEO_ID_2"></iframe>`;
-    } else {
-        container.innerHTML = `<iframe width="300" src="https://www.youtube.com/embed/VIDEO_ID_3"></iframe>`;
+    if(jenis === "abjad1") {
+        container.innerHTML = `<iframe width="400" height="250"
+        src="https://www.youtube.com/embed/VIDEO_ID_ABJAD1"
+        allowfullscreen></iframe>`;
+    } 
+    else if(jenis === "abjad2") {
+        container.innerHTML = `<iframe width="400" height="250"
+        src="https://www.youtube.com/embed/VIDEO_ID_ABJAD2"
+        allowfullscreen></iframe>`;
+    } 
+    else if(jenis === "angka1") {
+        container.innerHTML = `<iframe width="400" height="250"
+        src="https://www.youtube.com/embed/VIDEO_ID_ANGKA1"
+        allowfullscreen></iframe>`;
+    } 
+    else if(jenis === "angka2") {
+        container.innerHTML = `<iframe width="400" height="250"
+        src="https://www.youtube.com/embed/VIDEO_ID_ANGKA2"
+        allowfullscreen></iframe>`;
     }
-}
-
-if(document.getElementById("dataPeserta")) {
-    let peserta = JSON.parse(localStorage.getItem("peserta")) || [];
-    let tabel = document.getElementById("dataPeserta");
-
-    peserta.forEach(p => {
-        tabel.innerHTML += `<tr><td>${p.nama}</td><td>${p.email}</td></tr>`;
-    });
+    else if(jenis === "salam") {
+        container.innerHTML = `<iframe width="400" height="250"
+        src="https://www.youtube.com/embed/VIDEO_ID_SALAM"
+        allowfullscreen></iframe>`;
+    }
+    else if(jenis === "perkenalan") {
+        container.innerHTML = `<iframe width="400" height="250"
+        src="https://www.youtube.com/embed/VIDEO_ID_PERKENALAN"
+        allowfullscreen></iframe>`;
+    }
 }
